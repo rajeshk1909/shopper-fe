b0ccbee3e98006dd2feb95d02a03d6ac1a6dec46
rajeshk1909 <rajeshkumark1908@gmail.com>
2024-12-18 12:11:45 +0530

    UI Changes

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,6 +1,5 @@
-"use client"
 import Navbar from "@/components/Navbar"
-import React, { createContext, useEffect, useState } from "react"
+import React from "react"
 
 type layoutProps = {
   children: React.ReactNode
